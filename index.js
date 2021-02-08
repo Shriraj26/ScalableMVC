fa9e05ec3cd66771fff52d5853bf012591fb2517
Shriraj26 <syjcsayjcs@gmail.com>
2021-02-08 18:56:39 +0530

    Added Views

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const port = 8000;
 //Use the express router that we defined in router/index.js file
 app.use('/', require('./routes'));
 
+app.set('view engine', 'ejs');
+
+app.set('views', './views');
 
 
 app.listen(port, function(err){
